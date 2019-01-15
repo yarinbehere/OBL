a66da081e282748b84e4555896bdf7045756daa5
ramidman <ramidman@gmail.com>
2019-01-15 13:46:22 +0200

    Updated new fxmls

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
-// Omri is in the house
+
 public class Main extends Application {
 	Stage primaryStage;
 	VBox mainLayout;
