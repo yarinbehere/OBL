b9cda4da66de8e65f9e79075e1fa57935e9dad0b
שלו קובי < @ShalevSurface>
2019-01-24 22:19:30 +0200

    updated database again because previous commit didn't do it
    the database in the previous commit is of an old version, this is the
    newest one

--- DB/OBLDB.sql
+++ DB/OBLDB.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `obldb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE  IF NOT EXISTS `obldb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  */;
 USE `obldb`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
