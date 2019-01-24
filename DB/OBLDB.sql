070ed01ccb10294af0fee3f3f9edb2430859c1f8
shalev kubi <38704353+shalevku@users.noreply.github.com>
2019-01-24 22:22:26 +0200

    updated database again
     because previous commit didn't do it.
    the database in the previous commit is of an old version, this is the
    newest one

--- DB/OBLDB.sql
+++ DB/OBLDB.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `obldb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `obldb`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: localhost    Database: obldb
@@ -64,10 +66,10 @@ CREATE TABLE `bookorder` (
   `bookId` int(11) NOT NULL,
   `status` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`bookOrderNum`),
-  KEY `fk_BookOrder_reading cards1_idx` (`subscriptionNumber`),
   KEY `fk_BookOrder_books1_idx` (`bookId`),
+  KEY `fk_BookOrder_subscribers_idx` (`subscriptionNumber`),
   CONSTRAINT `fk_BookOrder_books1` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookid`),
-  CONSTRAINT `fk_BookOrder_reading cards1` FOREIGN KEY (`subscriptionNumber`) REFERENCES `readingcard` (`subscriptionnumber`)
+  CONSTRAINT `fk_BookOrder_subscribers` FOREIGN KEY (`subscriptionNumber`) REFERENCES `subscriber` (`subscriptionnumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -95,7 +97,7 @@ CREATE TABLE `borrowedbook` (
   `lostBook` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`bookId`,`subscriptionNumber`),
   KEY `bookFK_idx` (`bookId`),
-  KEY `fk_borrowedbook_subscriber1_idx` (`subscriptionNumber`),
+  KEY `fk_borrowedbook_subscriber_idx` (`subscriptionNumber`),
   CONSTRAINT `fk_borrowedbook_book` FOREIGN KEY (`bookId`) REFERENCES `book` (`bookid`),
   CONSTRAINT `fk_borrowedbook_subscriber` FOREIGN KEY (`subscriptionNumber`) REFERENCES `subscriber` (`subscriptionnumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -107,7 +109,7 @@ CREATE TABLE `borrowedbook` (
 
 LOCK TABLES `borrowedbook` WRITE;
 /*!40000 ALTER TABLE `borrowedbook` DISABLE KEYS */;
-INSERT INTO `borrowedbook` VALUES (1,1,'2019-01-23','2019-01-15',0),(3,4,'2019-01-10','2019-01-01',0),(8,6,'2019-01-02','2019-01-01',0);
+INSERT INTO `borrowedbook` VALUES (201,1,'2019-01-23','2019-01-15',0),(203,4,'2019-01-10','2019-01-01',0),(207,6,'2019-01-02','2019-01-01',0);
 /*!40000 ALTER TABLE `borrowedbook` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -208,12 +210,12 @@ CREATE TABLE `subscriber` (
   `userName` varchar(45) NOT NULL,
   `firstName` varchar(15) NOT NULL,
   `lastName` varchar(15) NOT NULL,
-  `phoneNumber` int(11) NOT NULL,
+  `phoneNumber` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `subscriberStatus` varchar(15) NOT NULL,
   PRIMARY KEY (`subscriptionNumber`),
-  KEY `fk_readingCard_user1_idx` (`userName`),
-  CONSTRAINT `fk_readingCard_user1` FOREIGN KEY (`userName`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_subscriber_user_idx` (`userName`),
+  CONSTRAINT `fk_subscriber_user` FOREIGN KEY (`userName`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -223,7 +225,7 @@ CREATE TABLE `subscriber` (
 
 LOCK TABLES `subscriber` WRITE;
 /*!40000 ALTER TABLE `subscriber` DISABLE KEYS */;
-INSERT INTO `subscriber` VALUES (1,'200','shalev','kubi',502171234,'shalevku@gmail.com','Locked'),(2,'201','yarin','belker',502272234,'yarin@gmail.com','Frozen'),(3,'202','hai','chasidi',502373234,'hai@gmail.com','Active'),(4,'203','omri','braymok',502474234,'omri@gmail.com','Active'),(5,'204','bibi','netanyahu',502575234,'bibi@gmail.com','Frozen'),(6,'205','roman','cohen',502676234,'roman@gmail.com','Locked'),(7,'206','michal','yanay',502777234,'michal@gmail.com','Frozen'),(8,'207','eyal','golan',502878234,'eyal@gmail.com','Active'),(9,'208','moshe','peretz',502979234,'moshe@gmail.com','Active');
+INSERT INTO `subscriber` VALUES (200,'200','shalev','kubi','0502171234','shalevku@gmail.com','Locked'),(201,'201','yarin','belker','0502272234','yarin@gmail.com','Frozen'),(202,'202','hai','chasidi','0502373234','hai@gmail.com','Active'),(203,'203','omri','braymok','0502474234','omri@gmail.com','Active'),(204,'204','bibi','netanyahu','0502575234','bibi@gmail.com','Frozen'),(205,'205','roman','cohen','0502676234','roman@gmail.com','Locked'),(206,'206','michal','yanay','0502777234','michal@gmail.com','Frozen'),(207,'207','eyal','golan','0502878234','eyal@gmail.com','Active'),(208,'208','moshe','perets','0502712993','moshe@gmail.com','Active');
 /*!40000 ALTER TABLE `subscriber` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -248,7 +250,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('100','aA123456','Librarian'),('101','skre544','Manager'),('102','Aa123456','Librarian'),('103','1010abba','Librarian'),('104','5646bird','Librarian'),('105','lock55','Librarian'),('106','Aa123456','Librarian'),('107','beholy1','Librarian'),('200','12345','Subscriber'),('201','Aa1342','Subscriber'),('202','654re','Subscriber'),('203','hey123','Subscriber'),('204','204204','Subscriber'),('205','0','Subscriber'),('206','1111','Subscriber'),('207','5454aA','Subscriber'),('208','Aa123456','Subscriber');
+INSERT INTO `user` VALUES ('100','aA123456','Librarian'),('101','skre544','Manager'),('102','Aa123456','Librarian'),('103','1010abba','Librarian'),('104','5646bird','Librarian'),('105','lock55','Librarian'),('106','Aa123456','Librarian'),('107','beholy1','Librarian'),('200','12345','Subscriber'),('201','Aa1342','Subscriber'),('202','654re','Subscriber'),('203','hey123','Subscriber'),('204','204204','Subscriber'),('205','0','Subscriber'),('206','1111','Subscriber'),('207','5454aA','Subscriber'),('208','gb9789','Subscriber');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -261,4 +263,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-24 12:11:32
+-- Dump completed on 2019-01-24 22:10:25
